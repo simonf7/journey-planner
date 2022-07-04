1cc9906cd95873b331306a73a4513377cb24b713
Simon <s.foster@tabs-software.co.uk>
2022-07-04 21:27:47 +0100

    [feature] convert function to class and flesh out structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      originText: '',
+      originLatitude: null,
+      originLongitude: null,
+      destinationText: '',
+      destinationLatitude: null,
+      destinationLongitude: null,
+      departureDate: null,
+      returnDate: null,
+    };
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <h1>Add your journey</h1>
+        </header>
+        <main classNae="app-main">
+          <div>
+            <h3>Origin</h3>
+          </div>
+          <div>
+            <h3>Destination</h3>
+          </div>
+          <div>
+            <h3>Departure</h3>
+          </div>
+          <div>
+            <h3>Return</h3>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
