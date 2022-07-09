df8ec5f94a3dd653a440cf43134e0eaed8694318
Simon <s.foster@tabs-software.co.uk>
2022-07-09 22:20:44 +0100

    [feature] add styling/layout and tidy code

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
       destinationLongitude: null,
       departureDate: Date(),
       returnDate: Date(),
+      submitting: false,
     };
 
     this.updateOrigin = this.updateOrigin.bind(this);
@@ -39,7 +40,30 @@ class App extends React.Component {
     });
   }
 
+  submitJourney() {
+
+  }
+
   render() {
+    const {
+      originText,
+      originLatitude,
+      originLongitude,
+      destinationText,
+      destinationLatitude,
+      destinationLongitude,
+      departureDate,
+      returnDate,
+      submitting,
+    } = this.state;
+
+    const submitDisabled =
+      !originLatitude ||
+      !originLongitude ||
+      !destinationLatitude ||
+      !destinationLongitude ||
+      !submitting;
+
     return (
       <div className="app">
         <header className="app-header">
@@ -48,72 +72,85 @@ class App extends React.Component {
 
         <main className="app-main">
           <aside className="app-aside">
-            <div>
-              <h3>Origin</h3>
-              <Location
-                id="origin"
-                placeholder="Enter origin location by name or coordinates below"
-                value={{
-                  text: this.state.originText,
-                  latitude: this.state.originLatitude,
-                  longitude: this.state.originLongitude,
-                }}
-                updateLocation={this.updateOrigin}
-              />
+            <div className="app-journey">
+              <div>
+                <h3>Journey Origin</h3>
+                <Location
+                  id="origin"
+                  placeholder="Enter origin location by name or coordinates below"
+                  value={{
+                    text: originText,
+                    latitude: originLatitude,
+                    longitude: originLongitude,
+                  }}
+                  updateLocation={this.updateOrigin}
+                />
+              </div>
+              <div>
+                <h3>Journey Destination</h3>
+                <Location
+                  id="destination"
+                  placeholder="Enter destination location by name or coordinates below"
+                  value={{
+                    text: destinationText,
+                    latitude: destinationLatitude,
+                    longitude: destinationLongitude,
+                  }}
+                  updateLocation={this.updateDestination}
+                />
+              </div>
+              <div>
+                <h3>Departure date/time</h3>
+                <DateTimePicker
+                  id="departure-datetime"
+                  locale="en-UK"
+                  value={departureDate}
+                  onChange={(value) => this.setState({ departureDate: value })}
+                />
+              </div>
+              <div>
+                <h3>Return date/time</h3>
+                <DateTimePicker
+                  id="return-datetime"
+                  locale="en-UK"
+                  value={returnDate}
+                  onChange={(value) => this.setState({ returnDate: value })}
+                />
+              </div>
             </div>
-            <div>
-              <h3>Destination</h3>
-              <Location
-                id="destination"
-                placeholder="Enter destination location by name or coordinates below"
-                value={{
-                  text: this.state.destinationText,
-                  latitude: this.state.destinationLatitude,
-                  longitude: this.state.destinationLongitude,
-                }}
-                updateLocation={this.updateDestination}
-              />
-            </div>
-            <div>
-              <h3>Departure</h3>
-              <DateTimePicker
-                id="departure-datetime"
-                locale="en-UK"
-                value={this.state.departureDate}
-                onChange={(value) => this.setState({ departureDate: value })}
-              />
-            </div>
-            <div>
-              <h3>Return</h3>
-              <DateTimePicker
-                id="return-datetime"
-                locale="en-UK"
-                value={this.state.returnDate}
-                onChange={(value) => this.setState({ returnDate: value })}
-              />
-            </div>
-            <div>
+            <div className="app-submit">
               <Button
+                id="submit-journey"
                 label="Submit Journey"
                 variant="brand"
                 className="rainbow-m-around_medium"
+                disabled={submitDisabled}
               />
             </div>
           </aside>
-          <section>
+          <section className="app-section">
             <Map
               origin={{
-                text: this.state.originText,
-                latitude: this.state.originLatitude,
-                longitude: this.state.originLongitude,
+                text: originText,
+                latitude: originLatitude,
+                longitude: originLongitude,
               }}
               destination={{
-                text: this.state.destinationText,
-                latitude: this.state.destinationLatitude,
-                longitude: this.state.destinationLongitude,
+                text: destinationText,
+                latitude: destinationLatitude,
+                longitude: destinationLongitude,
               }}
             />
           </section>
+          <div className="main-submit">
+            <Button
+              id="extra-submit-journey"
+              label="Submit Journey"
+              variant="brand"
+              className="rainbow-m-around_medium"
+              disabled={submitDisabled}
+            />
+          </div>
         </main>
       </div>
     );
